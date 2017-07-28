aa01266c79f6ef2e795c8edb8f2ad93f70899541
liyatao1991 <liyatao1991@126.com>
2017-07-28 13:42:46 +0800

    Initial Commit

---
+++ CustomNavAndTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomNavAndTabBar
+//
+//  Created by suge on 2017/7/28.
+//  Copyright © 2017年 郑州鹿客互联网科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomNavAndTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomNavAndTabBar
+//
+//  Created by suge on 2017/7/28.
+//  Copyright © 2017年 郑州鹿客互联网科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
